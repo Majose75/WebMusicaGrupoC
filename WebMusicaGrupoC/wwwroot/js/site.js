eb4c18e9d69dc346a36bc374d8d3760e106d5bc3
Majose <mariajose.paesa@gmail.com>
2024-05-23 14:42:32 +0200

    Añadido ordenacion por campos

--- WebMusicaGrupoC/wwwroot/js/site.js
+++ WebMusicaGrupoC/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$('th').click(function () {
+    var table = $(this).parents('table').eq(0)
+    var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+    this.asc = !this.asc
+    if (!this.asc) { rows = rows.reverse() }
+    for (var i = 0; i < rows.length; i++) { table.append(rows[i]) }
+})
+function comparer(index) {
+    return function (a, b) {
+        var valA = getCellValue(a, index), valB = getCellValue(b, index)
+        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+    }
+}
+function getCellValue(row, index) { return $(row).children('td').eq(index).text() }
